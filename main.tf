048da2bfcf953206041ca4aeac7a10e3b25e1263
Carlo Mencarelli <me@carlo.cloud>
2022-03-30 21:04:13 -0600

    Revert

--- main.tf
+++ main.tf
@@ -4,17 +4,81 @@ resource "tfe_organization" "carlo-cloud" {
   collaborator_auth_policy = "two_factor_mandatory"
 }
 
-resource "tfe_workspace" "terraform-aws-mq-broker" {
-  name              = "terraform-aws-mq-broker"
+resource "tfe_workspace" "aws-development" {
+  name              = "aws-development"
   organization      = tfe_organization.carlo-cloud.name
   terraform_version = var.terraform-version
-
-  vcs_repo {
-    identifier     = "mencarellic/terraform-aws-mq-broker"
-    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
-  }
+  tag_names         = ["aws", "dev"]
 }
 
-data "tfe_oauth_client" "github" {
-  oauth_client_id = "oc-hzaZFSBR6Dc6q2UW"
+resource "tfe_variable" "aws-development-key" {
+  key          = "secret_key"
+  value        = var.aws-dev-key
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.aws-development.id
+}
+
+resource "tfe_workspace" "azure-development" {
+  name              = "azure-development"
+  organization      = tfe_organization.carlo-cloud.name
+  terraform_version = var.terraform-version
+  tag_names         = ["azure", "dev"]
+}
+
+resource "tfe_variable" "azure-development-key" {
+  key          = "secret_key"
+  value        = var.azure-dev-key
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.azure-development.id
+}
+
+resource "tfe_workspace" "aws-production" {
+  name              = "aws-production"
+  organization      = tfe_organization.carlo-cloud.name
+  terraform_version = var.terraform-version
+  tag_names         = ["aws", "prod"]
+}
+
+resource "tfe_variable" "aws-production-key" {
+  key          = "secret_key"
+  value        = var.aws-prod-key
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.aws-production.id
+}
+
+resource "tfe_workspace" "azure-production" {
+  name              = "azure-production"
+  organization      = tfe_organization.carlo-cloud.name
+  terraform_version = var.terraform-version
+  tag_names         = ["azure", "prod"]
+}
+
+resource "tfe_variable" "azure-production-key" {
+  key          = "secret_key"
+  value        = var.azure-prod-key
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.azure-production.id
+}
+
+resource "tfe_workspace" "external-pagerduty" {
+  name              = "external-pagerduty"
+  organization      = tfe_organization.carlo-cloud.name
+  terraform_version = var.terraform-version
+  tag_names         = ["external"]
+}
+
+resource "tfe_workspace" "external-datadog" {
+  name         = "external-datadog"
+  organization = tfe_organization.carlo-cloud.name
+  tag_names    = ["external"]
+}
+
+resource "tfe_workspace" "external-github" {
+  name         = "external-github"
+  organization = tfe_organization.carlo-cloud.name
+  tag_names    = ["external"]
 }
