778d14f8892002eb620a551cf1673debcc4271d9
Carlo Mencarelli <me@carlo.cloud>
2023-03-15 21:39:51 -0700

    another workspace

--- workspace.tf
+++ workspace.tf
@@ -8,3 +8,14 @@ resource "tfe_workspace" "carlo-is" {
     oauth_token_id = data.tfe_oauth_client.mencarellic.oauth_token_id
   }
 }
+
+resource "tfe_workspace" "summit-cloud-solutions" {
+  name              = "summit-cloud-solutions"
+  organization      = tfe_organization.carlo-cloud.name
+  working_directory = "terraform"
+
+  vcs_repo {
+    identifier     = "mencarellic/summit-cloud-solutions"
+    oauth_token_id = data.tfe_oauth_client.mencarellic.oauth_token_id
+  }
+}
